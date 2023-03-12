7bbd1c6bc8d9f0b3dfa81ccd9891d856b0fab8a9
jayprakashlohar <103755935+jayprakashlohar@users.noreply.github.com>
2023-03-13 00:20:32 +0530

    added

--- mini-e-commerce/src/Components/Home/Home.jsx
+++ mini-e-commerce/src/Components/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { AutoSlider } from "./AutoSlider";
 import { MultipalSlider } from "./MultipalSlider";
 import { WatchSlider } from "./WatchSlider";
+import phone from "../../assets/phone.png";
 
 const Home = () => {
   return (
@@ -10,6 +11,14 @@ const Home = () => {
         <AutoSlider />
         <div style={{ margin: "10px 0px 25px 0px" }}>
           <div>
+            <div style={{ margin: "10px 0px 0px 0px", width: "100%" }}>
+              <img style={{ width: "100%" }} src={phone} alt="" />
+              <img
+                src="https://miro.medium.com/v2/resize:fit:1400/1*3LJmfM1VUuhn8oDghUlsYA.jpeg"
+                alt=""
+              />
+            </div>
+            <div style={{ padding: "5px", background: "black" }}></div>
             <img
               src="https://apple.directd.com.my/images/thumbs/0002161_Webslider-Promo-Feb-iPhone-14-Apple%20(1).jpeg"
               alt=""
