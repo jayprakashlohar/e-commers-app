ee683277d0785f55677721b6a750fd43a713bb31
jayprakashlohar <103755935+jayprakashlohar@users.noreply.github.com>
2023-03-12 22:21:01 +0530

    Added redme file

--- mini-e-commerce/src/Components/Home/Home.jsx
+++ mini-e-commerce/src/Components/Home/Home.jsx
@@ -9,13 +9,12 @@ const Home = () => {
       <div>
         <AutoSlider />
         <div style={{ margin: "10px 0px 25px 0px" }}>
-          <div style={{ margin: "0px 0px 10px 0px" }}>
+          <div>
             <img
               src="https://apple.directd.com.my/images/thumbs/0002161_Webslider-Promo-Feb-iPhone-14-Apple%20(1).jpeg"
               alt=""
             />
           </div>
-
           <div>
             <img
               src="https://apple.directd.com.my/images/thumbs/0002154_Webslider-Promo-Feb-iPhone-14-Pro-Apple.jpeg"
